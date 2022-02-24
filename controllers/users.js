8678b58d4cc3363cd54c2d9b688ada357f1b82ee
Tom Green <tom.green83@protonmail.com>
2022-02-24 19:40:31 +0000

    13.8 complete

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const { User } = require('../models/index')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  const user = await User.create(req.body)
+  res.json(user)
+})
+
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
+    }
+  })
+  user.username = req.body.username
+  user.save()
+  res.json(user)
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 const start = async () => {
   await connectToDatabase()
