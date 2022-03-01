7275b169f83fe797a9e2780d0fb2304e4f5eba01
Tom Green <tom.green83@protonmail.com>
2022-03-01 18:32:52 +0000

    13.22 complete

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,11 +1,24 @@
 const router = require('express').Router()
 const { Readinglist } = require('../models/index')
+const { tokenExtractor } = require('../utils/middleware')
 
 router.post('/', async (req, res) => {
-  // const readingList = await Readinglist.findAll()
   const readingList = await Readinglist.create({...req.body})
   console.log(readingList)
   res.json(readingList)
 })
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readingList = await Readinglist.findByPk(req.params.id)
+
+  if (!(req.decodedToken.id === readingList.userId)) {
+    res.status(400).send({ error: 'user is not creator of readinglist' })
+  }
+
+  readingList.read = req.body.read
+  await readingList.save()
+  res.json(readingList)
+})
+
 
 module.exports = router
